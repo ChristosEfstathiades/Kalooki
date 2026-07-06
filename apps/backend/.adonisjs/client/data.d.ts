8d9e7dc4c83584ef022806a8e08b74df172c7219
d86617ce <chrise3012@icloud.com>
2026-07-06 19:45:06 +0100

    bfgb

--- apps/backend/.adonisjs/client/data.d.ts
+++ apps/backend/.adonisjs/client/data.d.ts
@@ -5,6 +5,7 @@
 
 /// <reference path="./manifest.d.ts" />
 import type { InferData, InferVariants } from '@adonisjs/core/types/transformers'
+import type UserTransformer from '#transformers/user_transformer'
 import type ChatMessageTransformer from '#transformers/chat_message_transformer'
 import type FriendRequestTransformer from '#transformers/friend_request_transformer'
 import type GroupDetailTransformer from '#transformers/group_detail_transformer'
@@ -12,9 +13,12 @@ import type GroupInviteTransformer from '#transformers/group_invite_transformer'
 import type GroupTransformer from '#transformers/group_transformer'
 import type MatchTransformer from '#transformers/match_transformer'
 import type PublicUserTransformer from '#transformers/public_user_transformer'
-import type UserTransformer from '#transformers/user_transformer'
 
 export namespace Data {
+  export type User = InferData<UserTransformer>
+  export namespace User {
+    export type Variants = InferVariants<UserTransformer>
+  }
   export type ChatMessage = InferData<ChatMessageTransformer>
   export namespace ChatMessage {
     export type Variants = InferVariants<ChatMessageTransformer>
@@ -42,9 +46,5 @@ export namespace Data {
   export type PublicUser = InferData<PublicUserTransformer>
   export namespace PublicUser {
     export type Variants = InferVariants<PublicUserTransformer>
-  }
-  export type User = InferData<UserTransformer>
-  export namespace User {
-    export type Variants = InferVariants<UserTransformer>
   }
 }
